fa911edef0f8e5e531175f3af4cf983c59f2a43a
KonstantinovPA <p.a.kons@yandex.ru>
2024-04-10 12:09:19 +0300

    Roman number converter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,16 +9,48 @@ public class Main {
         String stringScan = "9 / 3";
 
 //        String[] words = inputString.split(" ");
-        String[] words = stringScan.split(" ");
-//test1
+        String[] splitWordsVariable = stringScan.split(" ");
         int number1 = 0;
         char operator = 0;
         int number2 = 0;
+
+        String romanNumber = "I";
+        if (romanNumber == "I") {
+            System.out.println(1);
+        }
+        if (romanNumber == "II") {
+            System.out.println(2);
+        }
+        if (romanNumber == "III") {
+            System.out.println(3);
+        }
+        if (romanNumber == "IV") {
+            System.out.println(4);
+        }
+        if (romanNumber == "V") {
+            System.out.println(5);
+        }
+        if (romanNumber == "VI") {
+            System.out.println(6);
+        }
+        if (romanNumber == "VII") {
+            System.out.println(7);
+        }
+        if (romanNumber == "VIII") {
+            System.out.println(8);
+        }
+        if (romanNumber == "IX") {
+            System.out.println(9);
+        }
+        if (romanNumber == "X") {
+            System.out.println(10);
+        }
+
         try {
-            number1 = Integer.parseInt(words[0]);
+            number1 = Integer.parseInt(splitWordsVariable[0]);
             System.out.println("Converted integer: " + number1);
-            operator = words[1].charAt(0);
-            number2 = Integer.parseInt(words[2]);
+            operator = splitWordsVariable[1].charAt(0);
+            number2 = Integer.parseInt(splitWordsVariable[2]);
             System.out.println("Converted integer: " + number2);
         } catch (NumberFormatException e) {
             System.out.println("Invalid integer input");
